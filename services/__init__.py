b4903cb847870399501f79d250b98b8b6afe105b
= <=>
2026-04-20 21:57:56 +0530

    update:(Rohit) update services initializer

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,7 @@
 from services.gambler_profile_service import GamblerProfileService
+from services.stake_management_service import StakeManagementService
 
 __all__ = [
 	"GamblerProfileService",
+	"StakeManagementService",
 ]
