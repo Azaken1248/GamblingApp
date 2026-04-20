837e79b9fd6ab700ce3b1cfd8fd1f6a1783408b1
= <=>
2026-04-20 23:50:53 +0530

    update:(Rohit) updated services initializer

--- services/__init__.py
+++ services/__init__.py
@@ -2,10 +2,12 @@ from services.gambler_profile_service import GamblerProfileService
 from services.betting_service import BettingService
 from services.game_session_manager import GameSessionManager
 from services.stake_management_service import StakeManagementService
+from services.win_loss_calculator import WinLossCalculator
 
 __all__ = [
 	"BettingService",
 	"GameSessionManager",
 	"GamblerProfileService",
 	"StakeManagementService",
+	"WinLossCalculator",
 ]
